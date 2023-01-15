382a34699d36f21d0228a9d1c3797e2312651a87
Ashkan <ashkan.majidi.1382@gmail.com>
2023-01-16 02:35:52 +0330

    Phase 1

---
+++ Phase1/Part3.py
@@ -0,0 +1,16 @@
+z1 = [1, 1, 1, 2, 3, 3, 2, 3, 2]
+z2 = [1, 1, 3, 2, 3, 1, 2, 3, 2]
+x = len(z1)
+print(x)
+
+
+def hamming(z1, z2):
+    ans = 0
+    n = len(z1)
+    for i in range(0, n):
+        if z1[i] != z2[i]:
+            ans += 1
+    return ans
+
+
+print(hamming(z1, z2))
