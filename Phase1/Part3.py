c563653b524c693d6824b524d8a2131d8b701927
Ashkan <ashkan.majidi.1382@gmail.com>
2023-01-18 14:47:36 +0330

    some small changes.

--- Phase1/Part3.py
+++ Phase1/Part3.py
@@ -20,18 +20,20 @@ def hamming(z1, z2):
 
 print(hamming(a, b))
 
+
 # 5
 def min_hamming(z1, z2):
     z1_values = list(pd.Series(z1).value_counts().index)
     permutations = list(itertools.permutations(z1_values))
-    min = 1000000000
+    min_ham = 1000000000
     for i in range(0, len(permutations)):
-        tmp = [0]*len(z2)
-        for j in range (0,len(z2)):
-            tmp[j]=permutations[i][z2[j]-1]
+        tmp = [0] * len(z2)
+        for j in range(0, len(z2)):
+            tmp[j] = permutations[i][z2[j] - 1]
         print(tmp)
-        if min > hamming(tmp, z1):
-            min = hamming(tmp,z1)
-    return min
+        if min_ham > hamming(tmp, z1):
+            min_ham = hamming(tmp, z1)
+    return min_ham
+
 
 min_hamming(a, b)
